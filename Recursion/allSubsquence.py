5ffbf3903886ac96691b8bd27a3ecd92a27182de
ABHEER MEHROTRA <72267713+abheer18@users.noreply.github.com>
2023-03-07 11:24:53 +0530

    all subsequence of a string

--- Recursion/allSubsquence.py
+++ Recursion/allSubsquence.py
@@ -1,16 +1,16 @@
-def printSubsequence(arr, indx, ds):
+def printSubsequence(arr, indx, mylist):
     # base case
     if indx == len(arr):
-        if len(ds) > 0:
-            print(ds)
+        if len(mylist) > 0:
+            print(mylist)
         return
 
-    ds.append(arr[indx])  # add to list
-    printSubsequence(arr, indx+1, ds)  # pick an element
-    ds.pop()  # remove from list
-    printSubsequence(arr, indx+1, ds)  # not pick
+    mylist.append(arr[indx])  # add to list
+    printSubsequence(arr, indx+1, mylist)  # pick an element
+    mylist.pop()  # remove from list
+    printSubsequence(arr, indx+1, mylist)  # not pick
 
 
 arr = [1, 2, 3]
-ds = []
-printSubsequence(arr, 0, ds)
+mylist    = []
+printSubsequence(arr, 0, mylist)
